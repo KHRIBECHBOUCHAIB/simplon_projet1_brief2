6de3baa77f17880aad79b63bbe76170f3e798b3f
simbouch <simplonbouchaib@gmail.com>
2024-09-27 12:33:32 +0200

    Update app.py with factorial function

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Hello, Simplon!")
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 5
+print(f"Factorial of {number} is {factorial(number)}")
